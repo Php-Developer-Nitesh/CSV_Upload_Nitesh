7670228c308efcec9b0cb649da71f3bec766ad19
Nitesh Kumar <niteshkumarranjan534@gmail.com>
2023-08-03 14:15:08 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://niteshkumarranjan534:nitesh123@cluster0.xnngdvj.mongodb.net/csv"
+);
+
+const db = mongoose.connection;
+
+db.once("open", function () {
+  console.log("database connected to the server successfully!");
+});
+
+module.exports = db;
